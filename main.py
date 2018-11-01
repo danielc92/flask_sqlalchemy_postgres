7fe16221e319c3070221f07fd27439b6d3e9faca
Daniel Corcoran <danielc.92g@gmail.com>
2018-11-01 15:06:27 +1100

    Restructured code using pep8, changed string response objects to jsonified responses.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import pandas
+from datetime import datetime as dt
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for, request, session, jsonify
+from flask_jsonpify import jsonpify
 '''
 Documentation for sql-flaskalchemy: 
 http://flask-sqlalchemy.pocoo.org/2.3/queries/#querying-records
@@ -8,16 +13,16 @@ Queries to set up database
 
 CREATE TABLE employees
 (
-	emp_id SERIAL PRIMARY KEY,
-	emp_name VARCHAR(255),
-	emp_age INTEGER
+    emp_id SERIAL PRIMARY KEY,
+    emp_name VARCHAR(255),
+    emp_age INTEGER
 )
 
 CREATE TABLE complaints
 (
-	complaint_id SERIAL PRIMARY KEY,
-	complaint_content VARCHAR(255),
-	complaint_emp_id INTEGER REFERENCES employees (emp_id)
+    complaint_id SERIAL PRIMARY KEY,
+    complaint_content VARCHAR(255),
+    complaint_emp_id INTEGER REFERENCES employees (emp_id)
 )
 '''
 
@@ -25,9 +30,6 @@ CREATE TABLE complaints
 '''
 Imports
 '''
-from flask import Flask, render_template, redirect, url_for, request, session
-from flask_sqlalchemy import SQLAlchemy
-import pandas
 
 '''
 Initiate flask app 
@@ -45,17 +47,18 @@ db = SQLAlchemy(app)
 '''
 Create mapper object to table in postgres database
 '''
+
+
 class Employee(db.Model):
 
-	__tablename__ = 'employees'
-	
-	emp_id = db.Column(db.Integer, primary_key = True)
-	emp_name = db.Column(db.String(255))
-	emp_age = db.Column(db.Integer)
+    __tablename__ = 'employees'
 
-	def __repr__(self):
-		return 'empid:{},emp_name:{},emp_age:{}'\
-		.format(self.emp_id, self.emp_name, self.emp_age)
+    emp_id = db.Column(db.Integer, primary_key=True)
+    emp_name = db.Column(db.String(255))
+    emp_age = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'empid:{self.emp_id},emp_name:{self.emp_name},emp_age:{self.emp_age}'
 
 
 '''
@@ -63,18 +66,29 @@ This route adds an new employee using url arguments.
 Usage Example:
 http://localhost:4999/add-employee?emp_name=daniel%20corcoran&emp_age=54
 '''
+
+
 @app.route('/add-employee')
 def add_new_employee():
-	try:
-		emp_name = request.args.get('emp_name')
-		emp_age = request.args.get('emp_age')
-		user = Employee(emp_name = emp_name, emp_age = emp_age)
-		db.session.add(user)
-		db.session.commit()
-		return 'Success'
-	
-	except Exception as e:
-		return 'Failed to add employee. Error: {}'.format(e)
+    try:
+        
+        emp_name = request.args.get('emp_name')
+        emp_age = request.args.get('emp_age')
+
+        if emp_name and emp_age:
+            
+            user = Employee(emp_name=emp_name, emp_age=emp_age)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({'Success':f'Inserted new record to database. {emp_name}, {emp_age}.'})
+        else:
+            
+            return jsonify({'Error':'emp_name and emp_age parameters must be populated.'})
+
+        
+
+    except Exception as e:
+        return jsonify({'Error':f'{e}'})
 
 
 '''
@@ -83,17 +97,19 @@ Note: Won't work if emp id exists in foreign tables
 Usage Example:
 http://localhost:4999/remove-employee?emp_id=16
 '''
+
+
 @app.route('/remove-employee')
 def remove_employee():
-	try:
-		emp_id = request.args.get('emp_id')
-		emp = Employee.query.filter_by(emp_id = emp_id).one()
-		db.session.delete(emp)
-		db.session.commit()
-		return 'Success'
-	
-	except Exception as e:
-		return 'Failed to remove employee. Error: {}'.format(e)
+    try:
+        emp_id = request.args.get('emp_id')
+        emp = Employee.query.filter_by(emp_id=emp_id).one()
+        db.session.delete(emp)
+        db.session.commit()
+        return jsonify({'Success':f'Removed employee with id {emp_id}'})
+
+    except Exception as e:
+        return jsonify({'Error':f'{e}'})
 
 
 '''
@@ -102,25 +118,35 @@ This route queries the employee table and returns a html table
 Usage Example:
 http://localhost:4999/show-employees-pandas
 '''
+
+
 @app.route('/show-employees-pandas')
 def show_employee_p():
-	try:
-		limit = request.args.get('limit')
+    try:
+        limit = request.args.get('limit')
 
-		if not limit:
-			limit = 9999999999999
+        if not limit:
+            limit = 9999999999999
 
-		print(limit)
+        data = pandas.read_sql(db.session.query(Employee).limit(limit).\
+                               with_entities(Employee.emp_id, 
+                                             Employee.emp_name, 
+                                             Employee.emp_age)\
+                               .statement, 
+                               con=db.session.bind)
 
-		data = pandas.read_sql(db.session.query(Employee).limit(limit).\
-			with_entities(Employee.emp_id, Employee.emp_name, Employee.emp_age)\
-			.statement, con = db.session.bind)		
+        format_ = request.args.get('format')
 
-		return data.to_html(index = False)
+        if format_ == 'html':
+            return data.to_html(index=False)
+        else:
+            response = data.to_json(orient='table')
+            return jsonify({'Success':response,
+                            'Timestamp': dt.now().strftime('%d-%m-%Y %H:%M:%S')})
 
-	except Exception as e:
-
-		return 'Failed to return data: error{}'.format(e)
+    except Exception as e:
+        response = jsonify({'Error':f'{e}'})
+        return response
 
 
 '''
@@ -128,21 +154,25 @@ This route queries the employee table and returns a query object string
 Usage Example:
 http://localhost:4999/show-employees-query
 '''
+
+
 @app.route('/show-employees-query')
 def show_employee_q():
-	try:
+    try:
 
-		query = Employee.query.all()
-		print(type(query))
-		return str(query)
-	except Exception as e:
+        query = Employee.query.all()
+        return str(query)
+    except Exception as e:
 
-		return 'Failed to return data: error{}'.format(e)
+        return jsonify({'Error':f'{e}'})
 
 
 '''
 Execute app on port 4999 in debug mode
 '''
+
+
 if __name__ == "__main__":
-	app.run(port = 4999,
-		debug = True)
+
+
+    app.run(port=4999, debug=True)
